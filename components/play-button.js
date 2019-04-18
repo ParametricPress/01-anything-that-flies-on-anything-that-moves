6b4eea4fa71eb97564d9cd63cac311562085b57a
Megan Vo <meganvo@cs.washington.edu>
2019-04-17 17:57:16 -0700

    wip play button

---
+++ components/play-button.js
@@ -0,0 +1,48 @@
+const React = require('react');
+
+class PlayButton extends React.Component {
+  play() {
+    // Pause if playing, play if paused
+    this.props.updateProps({
+      play: !this.props.play
+    });
+    console.log(this.props.play);
+
+    // If turned on, auto-increment timeline
+    if (this.props.play) {
+      this.interval = setInterval(() => {
+        if (this.props.month === 12) {
+          // Wrap around if month is Dec and increment year
+          var newYear = 1965 + ((this.props.year + 1 - 1965) % 9);
+          this.props.updateProps({
+            month: 1,
+            year: newYear
+          });
+        } else {
+          this.props.updateProps({
+            month: this.props.month + 1 // regularly update month
+          });
+        }
+      }, 1500);
+    } else {
+      clearInterval(this.interval);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { hasError, idyll, updateProps, ...props } = this.props;
+    return (
+      <div {...props}>
+        <button onClick={this.play.bind(this)}>
+          {this.props.play ? 'Pause' : 'Play'}
+        </button>
+      </div>
+    );
+  }
+}
+
+module.exports = PlayButton;
