c59c213497c655d973140eb8d44805967f86799f
Megan Vo <vvo.megan@gmail.com>
2019-04-18 22:49:54 -0700

    connect headline data to time

--- headline-data.js
+++ headline-data.js
@@ -2,6 +2,22 @@ export let headlines = [
   {
     headline: 'Operation Rolling Thunder Hits North Vietnam',
     paragraph:
-      'The 2nd of 5 theatres targeting North Vietnam. Large scale bombing that jumpstarts the war under LBJ. Original purpose is to "break the will of North Vietnam'
+      'The 2nd of 5 theatres targeting North Vietnam. Large scale bombing that jumpstarts the war under LBJ. Original purpose is to "break the will of North Vietnam',
+    month: 3,
+    year: 1965
+  },
+  {
+    headline: 'President Johnson Calls for Halt to Bombings in Address',
+    paragraph:
+      'President Johnson calls for a partial halt to bombing operations in Southeast Asia in a televised address. He announces his decision to not pursue a second term. Bombings in North Vietnam noticeably drop while missions are redirected to the Laotian panhandle.',
+    month: 3,
+    year: 1968
+  },
+  {
+    headline: 'Operation Rolling Thunder Ends',
+    paragraph:
+      'The three-year bombing operation over North Vietnam ends, lasting longer than US air involvement within Europe during the entirety of World War II. Rolling Thunder is regarded as a failure and had little effect in terms of whittling down North Vietnamese effort. Conventional literature estimates 864,000 tons of bombs dropped on North Vietnam thus far.',
+    month: 10,
+    year: 1968
   }
 ];
