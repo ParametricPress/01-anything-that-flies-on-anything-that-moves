ba659b4ae44a451c5cc30c3d31b318a8171a4668
Matthew Conlen <mc@mathisonian.com>
2019-04-21 21:31:14 -0700

    load data from all years. revamp map layout

--- scripts/wrangle-missions.js
+++ scripts/wrangle-missions.js
@@ -5,42 +5,40 @@ const roundToDecimalPlaces = (x, n) => {
   return Math.round(x * Math.pow(10, n)) / Math.pow(10, n)
 }
 
+const minYear = 1965;
+const maxYear = 1975;
 
-const results1 = [];
-const results2 = [];
+const results = {};
 
-// const cutoffDate = new Date('1969-01-01');
-const cutoffDate = new Date('1966-01-01');
-const headers = ['date', 'lat', 'lng'];
+for (var i = minYear; i <= maxYear; i++) {
+  results[i] = [];
+}
 
 fs.createReadStream(__dirname + '/../missions.csv')
   .pipe(csv())
   .on('data', (d) => {
-    // console.log(d);
+    if (d.MSNDATE.trim() === '') {
+      return;
+    }
     const date = new Date(d.MSNDATE);
+    const year = date.getFullYear();
 
-    if (date < cutoffDate) {
-      results1.push({
+    try {
+      results[+year].push({
         date: d.MSNDATE,
         lat: roundToDecimalPlaces(d.TGTLATDD_DDD_WGS84, 4),
         lng:roundToDecimalPlaces(d.TGTLONDDD_DDD_WGS84, 4)
       })
-    } else {
-
-      results2.push({
-        date: d.MSNDATE,
-        lat: roundToDecimalPlaces(d.TGTLATDD_DDD_WGS84, 4),
-        lng:roundToDecimalPlaces(d.TGTLONDDD_DDD_WGS84, 4)
-      })
+    } catch(e) {
+      console.log(year);
+      console.log(d.MSNDATE);
+      console.log(date.getFullYear());
     }
   })
   .on('end', () => {
-    const headers = Object.keys(results1[0]);
-
-    fs.writeFileSync('static/data/results1.csv', results1.map(d => {
-      return [d.date, d.lat, d.lng].join(',');
-    }).join('\n'))
-    // fs.writeFileSync('results2.csv', [headers.join(',')].concat(results2.map(d => {
-    //   return [d.date, d.lat, d.lng].join(',');
-    // })).join('\n'))
+    Object.keys(results).forEach((year) => {
+      fs.writeFileSync(`static/data/by-year/${year}.csv`, results[year].map(d => {
+        return [d.date, d.lat, d.lng].join(',');
+      }).join('\n'))
+    })
   });
