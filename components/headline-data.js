4434886ccaf27d1ba78854fed9388d2dcb8babe8
Megan Vo <vvo.megan@gmail.com>
2019-04-24 13:11:34 -0700

    update headlines

--- components/headline-data.js
+++ components/headline-data.js
@@ -3,8 +3,8 @@ export let headlines = [
     headline: 'Operation Rolling Thunder Underway in North Vietnam',
     paragraph:
       'Operation Rolling Thunder jumpstarts the war under President Lyndon B. Johnson in early March of 1965. Originally intended to "break the will of North Vietnam" with heavy bombing, Rolling Thunder eventually shifts aims to destroy the supply line of materials and men from the North to the South.',
-    startDay: 2,
-    startMonth: 10, // change this back to 3
+    startDay: 1,
+    startMonth: 10,
     startYear: 1965,
     endDay: 31, // last till end of month
     endMonth: 10,
@@ -17,14 +17,14 @@ export let headlines = [
     startDay: 25,
     startMonth: 12,
     startYear: 1965,
-    endDay: 5,
+    endDay: 20,
     endMonth: 1,
     endYear: 1966
   },
   {
     headline: 'President Johnson Calls for Halt to Bombings in Address',
     paragraph:
-      'President Johnson calls for a partial halt to bombing operations in Southeast Asia in a televised address, and announces his decision to not pursue a second term. Bombings in North Vietnam noticeably drop while missions are redirected to the Laotian panhandle.',
+      'President Johnson calls for a partial halt to bombing operations in Southeast Asia in a televised address and announces his decision to not pursue a second term. Bombings in North Vietnam noticeably drop while missions are redirected to the Laotian panhandle.',
     startDay: 31,
     startMonth: 3,
     startYear: 1968,
@@ -46,7 +46,7 @@ export let headlines = [
   {
     headline: 'Johnson Calls for Complete Halt of North Vietnam Bombings',
     paragraph:
-      'Due to pressure from home opposition, Johnson attempts to bring North Vietnam to the bargaining table by halting bombing operations in the area. All the leftover available sorties originally destined for North Vietnam divert to South Vietnam, the Ho Chi Minh Trail, and Northern Laos, which sees a sharp increase in bombing points. This secret redirection to Laos allows the US to avoid political scrutiny. \n Over the course of the entire year, data show that more than 200 bombings missions are flown to Laos on average per day.',
+      'Due to pressure from home opposition, Johnson attempts to bring North Vietnam to the bargaining table by halting bombing operations in the area. All the leftover available sorties originally destined for North Vietnam divert to South Vietnam, the Ho Chi Minh Trail, and Laos, which sees a sharp increase in bombing points. This secret redirection to Laos allows the US to avoid political scrutiny. \n Over the course of the entire year, data show that more than 200 bombings missions are flown to Laos on average per day.',
     startDay: 11,
     startMonth: 11,
     startYear: 1968,
@@ -134,6 +134,39 @@ export let headlines = [
     endYear: 1970
   },
   {
+    headline: 'November Records Missing Due to Damage',
+    paragraph:
+      'According to the THOR database FAQ, water damage to data archived in magnetic tapes causes a drop in recorded bombing points in November. During this month, General Westmoreland gives an optimistic forecast to the end of the Vietnam War and tells the US that the "end begins to come into view".',
+    startDay: 1,
+    startMonth: 11,
+    startYear: 1967,
+    endDay: 30,
+    endMonth: 11,
+    endYear: 1967
+  },
+  {
+    headline: 'The Tet Offensive Begins',
+    paragraph:
+      "Months after Westmoreland's optimistic assessment, the North Vietnamese and Viet Cong launch a surprise attack on numerous South Vietnamese targets, cities, and outposts. Meanwhile, news of the offensive causes US public support for the war to decline.",
+    startDay: 31,
+    startMonth: 1,
+    startYear: 1968,
+    endDay: 1,
+    endMonth: 3,
+    endYear: 1968
+  },
+  {
+    headline: 'The May Offensive Hits South Vietnam',
+    paragraph:
+      "The second phase of the Tet Offensive begins in early May. The US orders a series of heavy airstrikes to bombard North Vietnamese targets in the South, with some air sorties reportedly carrying 500-pound bombs of napalm. From May to June, almost one thousand B-52 sorties are flown 40 km within Saigon's downtown area. Although the US claims the heavy bombings were necessary, destruction of many towns and homes in South Vietnam due to the strikes generate controversy over the actual success of the operations.",
+    startDay: 4,
+    startMonth: 5,
+    startYear: 1968,
+    endDay: 30,
+    endMonth: 5,
+    endYear: 1968
+  },
+  {
     headline: 'Nixon Resumes Bombing in North Vietnam; Laos Bombing Decreases',
     paragraph:
       'In a transcript from the President to Attorney General John Mitchell, Nixon discusses resuming mass B-52 strikes over North Vietnam. "The bastards have never been bombed like they\'re going to be bombed this time," Nixon says on tape.',
@@ -147,7 +180,7 @@ export let headlines = [
   {
     headline: 'US Carpet Bombs North Vietnam in "Christmas Bombings"',
     paragraph:
-      'In an attempt to get North Vietnam return to the bargaining table, Nixon executes a highly publicized bombing of North Vietnam which becomes known as the "Christmas Bombings" or Operation Linebacker II. Over the course of 11 days, the US sends B-52s and heavily bombs the citizens of North Vietnam, only stopping to briefly suspend most operations on Christmas Day.',
+      'In an attempt to get North Vietnam to return to the bargaining table, Nixon executes a highly publicized bombing of North Vietnam which becomes known as the "Christmas Bombings" or Operation Linebacker II. Over the course of 11 days, the US sends B-52s to heavily bomb the citizens of North Vietnam, only stopping to briefly suspend most operations on Christmas Day.',
     startDay: 18,
     startMonth: 12,
     startYear: 1972,
@@ -191,7 +224,7 @@ export let headlines = [
   {
     headline: 'Operation Freedom Deal in Cambodia Ends',
     paragraph:
-      'After months of extensive bombing, Operation Freedom Deal ends.  Overall, the bombings are considered a tactical failure and instead are used as propaganda fodder by the Khmer Rouge to recruit members. Previous estimates by Kiernan state that between 50,000 and 150,000 civilians are killed during the campaigns in Cambodia; however, with the updated estimates, he suggests that the figures are much higher.',
+      'After months of extensive bombing, Operation Freedom Deal ends due to pressure from Congress.  Overall, the effectiveness of the bombings are highly disputed. Sydney Schanberg—a former New York Times correspondent and Pulitzer Prize winner for his coverage on Cambodia—suggests that the bombs were used as propaganda by the Khmer Rouge to recruit members. Moreover, previous estimates by researchers state that between 50,000 and 150,000 civilians were killed during the campaigns in Cambodia; however, with the updated estimates, it is suggested that the figures are much higher.',
     startDay: 15,
     startMonth: 8,
     startYear: 1973,
