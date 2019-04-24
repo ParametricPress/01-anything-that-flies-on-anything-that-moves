4875741d658def79d65b8a16be3c80261c6a00af
Megan Vo <vvo.megan@gmail.com>
2019-04-24 00:30:08 -0700

    add more headlines

--- components/headline-data.js
+++ components/headline-data.js
@@ -24,24 +24,90 @@ export let headlines = [
   {
     headline: 'Operation Rolling Thunder Ends',
     paragraph:
-      'The three-year bombing operation over North Vietnam ends, lasting longer than US air involvement within Europe during the entirety of World War II. Rolling Thunder is regarded as a failure and had little effect in terms of whittling down North Vietnamese effort. Conventional literature estimates 864,000 tons of bombs dropped on North Vietnam thus far.',
-    startDay: 31,
-    startMonth: 10,
+      'The three-year bombing operation over North Vietnam ends, lasting longer than US air involvement within Europe during the entirety of World War II. Rolling Thunder is regarded as a failure and has little effect in terms of whittling down North Vietnamese effort. Research estimates that 864,000 tons of bombs were dropped on North Vietnam thus far.',
+    startDay: 1,
+    startMonth: 11,
     startYear: 1968,
-    endDay: 30,
+    endDay: 10,
     endMonth: 11,
     endYear: 1968
   },
   {
-    headline:
-      'Johnson Calls for Complete Halt of North Vietnam Bombings; Operation Steel Tiger ends',
+    headline: 'Johnson Calls for Complete Halt of North Vietnam Bombings',
     paragraph:
-      'Due to pressure from home opposition, Johnson attempts to bring North Vietnam to the bargaining table by halting bombing operations in the area. All the leftover available sorties originally destined for North Vietnam divert to South Vietnam, the Ho Chi Minh Trail, and Northern Laos, which sees a sharp increase in bombing points. This secret redirection to Laos allows the US to avoid political scrutiny. Over the course of the entire year, data show that more than 200 bombings missions are flown to Laos on average per day.',
+      'Due to pressure from home opposition, Johnson attempts to bring North Vietnam to the bargaining table by halting bombing operations in the area. All the leftover available sorties originally destined for North Vietnam divert to South Vietnam, the Ho Chi Minh Trail, and Northern Laos, which sees a sharp increase in bombing points. This secret redirection to Laos allows the US to avoid political scrutiny. \n Over the course of the entire year, data show that more than 200 bombings missions are flown to Laos on average per day.',
     startDay: 11,
     startMonth: 11,
     startYear: 1968,
-    endDay: 30,
-    endMonth: 11,
+    endDay: 1,
+    endMonth: 12,
     endYear: 1968
+  },
+  {
+    headline: 'Nixon Becomes President',
+    paragraph:
+      'Richard M. Nixon swears into office and becomes the 37th President of the United States. Despite later pledging to honorably exit the Vietnam War, the war under Nixon continues for another three years and expands further into Cambodia.',
+    startDay: 20,
+    startMonth: 1,
+    startYear: 1969,
+    endDay: 31,
+    endMonth: 1,
+    endYear: 1969
+  },
+  {
+    headline: 'Nixon Orders Secret Carpet Bombings Over Cambodia',
+    paragraph:
+      'A few months into office, Nixon resumes the air warfare in Cambodia that Johnson began under "Operation Menu". As the new President, Nixon secretly authorizes the use of B-52 heavy bombers and escalates from tactical to mass carpet bombings over the country in order to root out North Vietnamese forces. Contrary to his aims, this only pushes North Vietnam further into Cambodia. Researchers note that many bomb coordinates in the database that hit Cambodia during this time are likely mislabeled as hitting South Vietnam, suggesting falsfication of records.',
+    startDay: 18,
+    startMonth: 3,
+    startYear: 1969,
+    endDay: 31,
+    endMonth: 3,
+    endYear: 1969
+  },
+  {
+    headline: 'Prince Sihanouk Falls, Lon Nol Rises',
+    paragraph:
+      'General Lon Nol succeeds in overthrowing Prince Sihanouk of Cambodia. Under the US-backed leader, Cambodian neutrality in the Vietnam War ends, and Cambodia demands North Vietnamese forces to remove themselves from camps set up along the border. North Vietnam instead responds by extending its presence in Cambodia.',
+    startDay: 18,
+    startMonth: 3,
+    startYear: 1970,
+    endDay: 31,
+    endMonth: 3,
+    endYear: 1970
+  },
+  {
+    headline:
+      'Nixon Orders a Joint US and South Vietnamese Attack in Cambodia; Protests Erupt in Response',
+    paragraph:
+      'After the fall of Prince Sihanouk, Nixon authorizes a joint invasion into Cambodia and announces the "incursion" in a televised address. The public announcement sparks a nationwide protest, despite Johnson\'s secret involvement with Cambodia years before. Days after the address, the Kent State shootings occur.',
+    startDay: 30,
+    startMonth: 4,
+    startYear: 1970,
+    endDay: 15,
+    endMonth: 5,
+    endYear: 1970
+  },
+  {
+    headline: 'Operation Menu Ends; Operation Freedom Deal Begins',
+    paragraph:
+      'Operation Menu ends in late May, culminating in 3,857 sorties flown and an estimated 108,823 tons of bombs dropped on Cambodia. Operation Freedom Deal picks up where Operation Menu leaves off and expands air warfare in Cambodia. The operation—which is aimed to provide support for Lon Nol against the rising Khmer Rouge—is supposedly confined to a 48 km region. However, records show that 3,634 sorties are flown outside of this boundary line.',
+    startDay: 19,
+    startMonth: 5,
+    startYear: 1970,
+    endDay: 1,
+    endMonth: 6,
+    endYear: 1970
+  },
+  {
+    headline: 'Nixon Resumes Bombing in North Vietnam; Laos Bombing Decreases',
+    paragraph:
+      'In a transcript from the President to Attorney General John Mitchell, Nixon discusses resuming mass B-52 strikes over North Vietnam. "The bastards have never been bombed like they\'re going to be bombed this time,\' Nixon says on tape."',
+    startDay: 4,
+    startMonth: 4,
+    startYear: 1972,
+    endDay: 31,
+    endMonth: 4,
+    endYear: 1972
   }
 ];
